d60ac3add09ed4da297e8052c94bae390cae0a22
piscina12000 <gabrielpissininmenossi@gmail.com>
2024-09-27 10:16:51 -0300

    testar gh-pages 4

--- src/Componentes/Telas/DetalhesCandidato.jsx
+++ src/Componentes/Telas/DetalhesCandidato.jsx
@@ -1,11 +1,12 @@
 import { Container } from "react-bootstrap";
+import Candidato from "./elementos/Candidato";
 
 export default function DetalhesCandidato(props) {
     return (
         <Container>
             <h1>Detalhes Candidato</h1>
-            { //...
-                
+            {
+                <Candidato candidato={props.candidato}></Candidato>
             }
         </Container>
     );
